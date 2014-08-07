3890938c1fe4b1aa8d01df38c6fd4c64e8ba6c0a
Akira Panda <p.chenliang@gmail.com>
2014-08-07 15:58:16 +0800

    Add all

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-2.0.3.min
+//= require jquery.easing-1.3.min.js
+//= require modernizr.js
+//= require superfish.js
+//= require jquery.carouFredSel-6.2.1-packed.js
+//= require jquery.themepunch.plugins.min.js
+//= require jquery.themepunch.revolution.min.js
+//= require mediaelement-and-player.min.js
+//= require jquery.fancyboxe45f.js
+//= require jquery.fancybox-media2c70.js
+//= require jquery.donutchart.js
+//= require jflickrfeed.min.js
+//= require jquery.twitter.js
+//= require accordion-functions.js
+//= require theme-functions.js
+//= require jquery.retina.js
+//= require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewnilHomepage::Application.config.secret_key_base = '33b62ec27d2686a4befc30b29a0a1aa375bc4a9836e7813644f23d3e10e7aeacb308f9e9d64195ce0e19d6f66b8f5ac48aba6e88841ebc129f600d5339574379'
